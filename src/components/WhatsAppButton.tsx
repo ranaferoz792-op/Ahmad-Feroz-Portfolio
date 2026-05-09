02db3c33e72275ba94a4bd95867a394f6f9d3903
ranaferoz792-op <ranaferoz792@gmail.com>
2026-05-09 16:31:50 +0500

    Updates Contacts or others

---
+++ src/components/WhatsAppButton.tsx
@@ -0,0 +1,16 @@
+import { FaWhatsapp } from "react-icons/fa";
+
+const WhatsAppButton = () => {
+  return (
+    <a
+      href="https://wa.me/923238475516"
+      target="_blank"
+      rel="noopener noreferrer"
+      className="fixed right-4 bottom-6 z-50 bg-green-500 hover:bg-green-600 text-white p-4 rounded-full shadow-lg transition-all duration-300 hover:scale-110"
+    >
+      <FaWhatsapp size={30} />
+    </a>
+  );
+};
+
+export default WhatsAppButton;
