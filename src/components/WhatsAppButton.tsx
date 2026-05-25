4f00ec989927e016940bba3d03444f936e616356
ranaferoz792-op <ranaferoz792@gmail.com>
2026-05-25 13:03:32 +0500

    Updates

--- src/components/WhatsAppButton.tsx
+++ src/components/WhatsAppButton.tsx
@@ -3,7 +3,7 @@ import { FaWhatsapp } from "react-icons/fa";
 const WhatsAppButton = () => {
   return (
     <a
-      href="https://wa.me/923238475516"
+         href="https://wa.me/923238475516?text=Hi%20Ahmad%20%20I%20just%20viewed%20your%20portfolio%20and%20I’m%20interested%20in%20your%20web%20development%20services."
       target="_blank"
       rel="noopener noreferrer"
       className="fixed right-4 bottom-6 z-50 bg-green-500 hover:bg-green-600 text-white p-4 rounded-full shadow-lg transition-all duration-300 hover:scale-110"
